7b71b7ab996365b9b88665560c047bf7d51eb0b9
Pelifix <100868053+Pelifix-techprogrmas@users.noreply.github.com>
2023-02-17 21:36:09 +0100

    Upload of the Work

---
+++ vrp_dmvschool/__resource.lua
@@ -0,0 +1,35 @@
+resource_manifest '44febabe-d386-4d18-afbe-5e627f4af937'
+
+ui_page 'html/ui.html'
+dependency 'vrp'
+files {
+	'html/ui.html',
+	'html/logo.png',
+	'html/dmv.png',
+	'html/cursor.png',
+	'html/styles.css',
+	'html/questions.js',
+	'html/scripts.js',
+	'html/debounce.min.js'
+}
+
+server_scripts {
+	'@vrp/lib/utils.lua',
+	'server.lua'
+}
+
+client_script {
+	'client.lua',
+	'GUI.lua'
+}
+
+--[[
+  ____             ___                ___                 
+ /\  _`\          /\_ \      __     /'___\  __            
+ \ \ \L\ \   __   \//\ \    /\_\   /\ \__/ /\_\    __  _  
+  \ \ ,__/ /'__`\   \ \ \   \/\ \  \ \ ,__\\/\ \  /\ \/'\ 
+   \ \ \/ /\  __/    \_\ \_  \ \ \  \ \ \_/ \ \ \ \/>  </ 
+    \ \_\ \ \____\   /\____\  \ \_\  \ \_\   \ \_\ /\_/\_\
+     \/_/  \/____/   \/____/   \/_/   \/_/    \/_/ \//\/_/
+                                                                                                                 
+]]--
